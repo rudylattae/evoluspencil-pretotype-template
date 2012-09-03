810ba3566a491e29341bd9b44eae108f61a898e1
Rudy Lattae <rudylattae@gmail.com>
2012-09-02 22:23:37 -0600

    Capable of generating clickable pretotype with the help of Path.js. Build and release v0.2-a

---
+++ EvolusPencil-PretotypeTemplate/Resources/main.js
@@ -0,0 +1,14 @@
+var $ = function(a,b){a=a.match(/^(\W)?(.*)/);return(b||document)["getElement"+(a[1]?a[1]=="#"?"ById":"sByClassName":"sByTagName")](a[2])};
+
+Path.map("#/page/:page_id").to(function(){
+    var page_id = this.params['page_id'];
+    var pages = $('.page');
+    for(i=0; i<pages.length; i++) {
+        pages[i].hidden = true;
+    }
+    $('#'+page_id).hidden = false;
+});
+
+Path.root("#/page/start");
+
+Path.listen();
